da98c57042c9b89a21aff00c0f6b9b4cc3ef226c
chenliuchun <chen_liuchun@qq.com>
2017-05-02 17:38:54 +0800

    init

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^index/$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page)
+
+]
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from . import models
+
+
+def index(request):
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', {'articles': articles})
+
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article': article})
